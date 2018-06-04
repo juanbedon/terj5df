28fbdd4a29bbea5891a6846163c9aec76279dac1
Juan <juanbedon26@hotmail.com>
2018-06-04 12:45:42 -0500

    Funcionalidad ejercicio

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = { guests: [], name: '', surname: '' }
+
+    this.newGuest = this.newGuest.bind(this);
+    this.firstName = this.firstName.bind(this);
+    this.lastName = this.lastName.bind(this)
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.newGuest}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input type="text" className="form-control" name="first-name" value={this.state.name} onChange={this.firstName}/>
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input type="text" className="form-control" name="last-name" value={this.state.surname} onChange={this.lastName}/>
               </div>
 
               <div className="action">
@@ -30,7 +41,12 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+                { this.state.guests.map((guest) =>
+                  <tr>
+                    <td>{guest.name}</td>
+                    <td>{guest.surname}</td>
+                  </tr>
+                )}
               </tbody>
             </table>
           </div>
@@ -38,8 +54,24 @@ class App extends Component {
       </div>
     )
   }
+
+  firstName(e) {
+    this.setState({ name: e.target.value});
+  }
+
+  lastName(e) {
+    this.setState({ surname: e.target.value});
+  }
+
+  newGuest(e) {
+    this.setState({
+      guests: this.state.guests.concat({ name: this.state.name, surname: this.state.surname }),
+      name: '',
+      surname: '',
+    });
+    e.preventDefault();
+  }
+
 }
 
 export default App
-
-
